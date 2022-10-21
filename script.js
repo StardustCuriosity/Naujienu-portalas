391fa8d34a05dfe0b31e3e9151db705433879bc2
StardustCuriosity <laurakvedaraite@yahoo.com>
2022-10-21 15:08:12 +0300

    Prideti: prenumeratos mygtukas ir prenumeratos forma

---
+++ script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("prenumeratosForma").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("prenumeratosForma").style.display = "none";
+  }
