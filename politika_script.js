605372943685eca6963902d5978513432c3395c4
DoviJas <114560514+DoviJas@users.noreply.github.com>
2022-10-23 14:53:18 +0300

    Įkeltas script'o kodas time read

---
+++ politika_script.js
@@ -0,0 +1,35 @@
+function readingTime() {
+    const text = document.getElementById("poli1").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time").innerText = time;
+  }
+  readingTime();
+
+  function readingTime2() {
+    const text = document.getElementById("poli2").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time2").innerText = time;
+  }
+  readingTime2();
+
+function readingTime3() {
+    const text = document.getElementById("poli3").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time3").innerText = time;
+  }
+  readingTime3();
+
+  function readingTime4() {
+    const text = document.getElementById("poli4").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time4").innerText = time;
+  }
+  readingTime4();
